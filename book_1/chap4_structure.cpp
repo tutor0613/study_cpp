da2d801702a9c5aecd485bcc582287e1b4b79dd0
tutor0613 <tutor0613@github.com>
2023-02-09 17:57:49 +0900

    230209 : Add chapter4 - struct & ptr

---
+++ book_1/chap4_structure.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+struct person
+{
+  std::string name;
+  int age;
+  char gender[2];
+};
+
+struct widget
+{
+  std::string brand;
+  int type;  // 1:int, 2:str
+  union  // anonymous union => 잘 안됨 쓰지말자 !!
+  {
+    long id_num;
+    char id_char[20];
+  };
+};
+
+enum spectrum {red, orange, yellow, green, blue, indigo, violet};
+
+int main()
+{
+  person yp {"Youngpyeong Kim", 28, "M"};
+
+  std::cout << "name : " << yp.name << " , age : " << yp.age << " , gender : " << yp.gender << " \n";
+  // std::cout << "gender[0] : " << yp.gender[0] << " , gender[1] : " << yp.gender[1] << " \n";
+
+  spectrum color[3] {red, green, blue};
+
+  std::cout << color[0] << " " << color[1] << " " << color[2] << " \n";
+
+  return 0;
+}
