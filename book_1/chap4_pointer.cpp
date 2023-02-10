624d0bcfd154b99fbbde1af8305bc054c083ee56
tutor0613 <tutor0613@github.com>
2023-02-10 12:02:34 +0900

    230210 : Update chapter4 - pointer

--- book_1/chap4_pointer.cpp
+++ book_1/chap4_pointer.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
 
 
+int main()
+{
+  int a = 13;
+  double b = 1.2345;
+  std::string c = "ASDFASDF";
+  char d[2] = "d";
+
+  std::cout << "a : " << a << " , &a : " << &a << " \n";
+  std::cout << "b : " << b << " , &b : " << &b << " \n";
+  std::cout << "c : " << c << " , &c : " << &c << " \n";
+  std::cout << "d : " << d << " , &d : " << &d << " \n";
+
+  int* a_ptr = &a;
+  double* b_ptr = &b;
+  std::string* c_ptr = &c;
+
+  std::cout << "*a_ptr : " << *a_ptr << " , a_ptr : " << a_ptr << " \n"; 
+  std::cout << "*b_ptr : " << *b_ptr << " , b_ptr : " << b_ptr << " \n"; 
+  std::cout << "*c_ptr : " << *c_ptr << " , c_ptr : " << c_ptr << " \n"; 
+
+  return 0;
+}
